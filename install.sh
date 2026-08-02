5d156d1455d5479669c67e0070c6247e1fbfcbad
JackAltman <cs2dtzq@163.com>
2026-08-02 15:02:35 +0800

    treewide: changed name to -debian
    Signed-off-by: JackAltman <cs2dtzq@163.com>

--- install.sh
+++ install.sh
@@ -1130,7 +1130,7 @@ install_latest_version() {
     LATEST_TAG_NAME=$(echo "$RELEASE_DATA" | jq -r --arg filter "$arch_filter" --arg profile "$profile" '
       map(
         select(.tag_name | test("^" + $filter + "-[0-9]"; "i"))
-        | select(if $profile == "max" then (.tag_name | endswith("-debian-server-max")) else (.tag_name | endswith("-debian-server")) end)
+        | select(if $profile == "max" then (.tag_name | endswith("-debian-max")) else (.tag_name | endswith("-debian")) end)
       )
       | sort_by(.published_at)
       | .[-1].tag_name
@@ -1190,7 +1190,7 @@ install_specific_version() {
     MATCH_TAGS=$(echo "$RELEASE_DATA" | jq -r --arg filter "$arch_filter" --arg profile "$profile" '
       .[]
       | select(.tag_name | test("^" + $filter + "-[0-9]"; "i"))
-      | select(if $profile == "max" then (.tag_name | endswith("-debian-server-max")) else (.tag_name | endswith("-debian-server")) end)
+      | select(if $profile == "max" then (.tag_name | endswith("-debian-max")) else (.tag_name | endswith("-debian")) end)
       | .tag_name
     ')
 
@@ -1251,6 +1251,7 @@ echo -e "\033[36m当前队列管理算法：    \033[0m\033[1;32m$CURRENT_QDIS
 print_separator
 echo -e "\033[1;33m作者：Joey  |  博客：https://joeyblog.net  |  反馈群组：https://t.me/+ft-zI76oovgwNmRh\033[0m"
 echo -e "\033[1;33m改版作者：JackA1ltman\033[0m"
+echo -e "\033[1;33m仅限Debian设备安装使用\033[0m"
 print_separator
 
 echo -e "\033[1;33m╭( ･ㅂ･)و ✧ 你可以选择以下操作哦：\033[0m"
